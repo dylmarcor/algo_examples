4431ccb51a4efbcfcc4a0c31fe002058dac09894
dylmarcor <dylancorbett@icloud.com>
2021-10-17 17:56:48 -0700

    new Python O(log n) script

---
+++ O_log_n.py
@@ -0,0 +1,16 @@
+import random
+import datetime
+
+def binary_searcher(search_key, arr):
+    low = 0
+    high = len(arr)-1
+    while low <= high:
+        mid = int(low + (high-low)//2)
+        if search_key == arr[mid]:
+            return True
+        if search_key < arr[mid]:
+            high = mid-1
+        elif search_key > arr[mid]:
+            low = mid+1
+
+return False
